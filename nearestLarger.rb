0a2811f18b5fdcb8c4450e6b431b6bf58155be1c
Travis Delly <dellybro@gmail.com>
2015-03-02 16:10:51 -0700

    Create nearestLarger.rb

---
+++ nearestLarger.rb
@@ -0,0 +1,15 @@
+def nearest_larger(arr, idx)
+  while true
+    if arr[idx-1] > arr[idx]
+      return idx-1
+    elsif arr[idx+1] > arr[idx]
+      return idx+1
+    else
+      return nil
+    end
+  end
+
+end
+
+
+puts nearest_larger([2,3,4,8], 2) == 3

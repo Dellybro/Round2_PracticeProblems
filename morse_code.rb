9258fa918521ef0f4935a2844948b2477112a947
Travis Delly <dellybro@gmail.com>
2015-03-04 18:53:20 -0700

    Update morse_code.rb

--- morse_code.rb
+++ morse_code.rb
@@ -31,11 +31,26 @@ def morse_encode(str)
   idx = 0
   result = ""
   while idx < str.length
-    result << "  " if str[idx] == " "
+    result << " " if str[idx] == " "
     result << MORSE_CODE[str[idx]].to_s << " " unless str[idx] == " "
     idx+=1
   end
   result
 end
 
+
+def morse(str)
+  words = str.split("")
+  encodedWords = words.map {|i| encoded(i)}
+  encodedWords.join(" ")
+
+
+end
+def encoded(i)
+  letters = i.split("")
+  encodedLetters = letters.map{|l| MORSE_CODE[l]}
+  encodedLetters.join(" ")
+end
+
+puts morse("cat in hat")
 puts morse_encode("cat in hat")
